667a48625cef85b69d99269fa75c34f97645ab61
yoonsungj04 <y213087@dac.unicamp.br>
2024-06-24 12:47:18 -0300

    modified:   arquivos_matrizes/matrizD.dat 	modified:   arquivos_matrizes/matrizE.dat 	modified:   main 	modified:   main.c

--- main.c
+++ main.c
@@ -1,58 +1,116 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int abrindoarquivos(const char* path, int** matrix, int matrixSize) {
-    FILE *file = fopen(path, "r");
-    if (file == NULL) {
+int abrirArquivos(const char* caminho, int** matriz, int tamanhoMatriz) {
+    FILE *arquivo = fopen(caminho, "r");
+    if (arquivo == NULL) {
         return 0;
     }
 
-    *matrix = (int*)malloc(matrixSize * sizeof(int));
-    if (*matrix == NULL) {
-        fclose(file);
+    *matriz = (int*)malloc(tamanhoMatriz * tamanhoMatriz * sizeof(int));
+    if (*matriz == NULL) {
+        fclose(arquivo);
         return 0;
     }
 
-    for (int i = 0; i < matrixSize; i++) {
-        fscanf(file, "%d", &(*matrix)[i]);
-    }
-
-    fclose(file);
-    return 1;
-}
-
-int somaMatrizesAeB(int *matrixA, int *matrixB, int matrixSize, const char *filename) {
-    FILE *file = fopen(filename, "w");
-    if (file == NULL) {
-        printf("Erro ao abrir o arquivo para escrita.\n");
-        return 0;
-    }
-
-    for (int i = 0; i < matrixSize; i++) {
-        int sum = matrixA[i] + matrixB[i];
-        fprintf(file, "%d\n", sum);
-    }
-
-    fclose(file);
-    return 1;
-}
-
-int main() {
-    int *matrixA, *matrixB;
-    int matrixSize = 1000;
-
-    if (!abrindoarquivos("arquivos_matrizes/matrizA.dat", &matrixA, matrixSize) ||
-        !abrindoarquivos("arquivos_matrizes/matrizB.dat", &matrixB, matrixSize)) {
-        printf("Erro ao abrir um ou mais arquivos.\n");
-    } else {
-
-        if (!somaMatrizesAeB(matrixA, matrixB, matrixSize, "arquivos_matrizes/matrizD.dat")) {
-            printf("Erro ao somar as matrizes e escrever no arquivo.\n");
+    for (int i = 0; i < tamanhoMatriz * tamanhoMatriz; i++) {
+        if (fscanf(arquivo, "%d", &(*matriz)[i]) != 1) {
+            free(*matriz);
+            fclose(arquivo);
+            return 0;
         }
     }
 
-    free(matrixA);
-    free(matrixB);
+    fclose(arquivo);
+    return 1;
+}
+
+int escreverMatrizNoArquivo(int *matriz, int tamanhoMatriz, const char *nomeArquivo) {
+    FILE *arquivo = fopen(nomeArquivo, "w");
+    if (!arquivo) {
+        perror("Falha ao abrir arquivo para escrita");
+        return 0;
+    }
+
+    for (int i = 0; i < tamanhoMatriz; i++) {
+        for (int j = 0; j < tamanhoMatriz; j++) {
+            if (fprintf(arquivo, "%d ", matriz[i * tamanhoMatriz + j]) < 0) {
+                fclose(arquivo);
+                return 0;
+            }
+        }
+        fprintf(arquivo, "\n");
+    }
+
+    fclose(arquivo);
+    return 1;
+}
+
+int somarMatrizes(int *matriz1, int *matriz2, int tamanhoMatriz, const char *nomeArquivo) {
+    int *matrizResultado = (int *)malloc(tamanhoMatriz * tamanhoMatriz * sizeof(int));
+    if (matrizResultado == NULL) {
+        printf("Falha na alocação de memória.\n");
+        return 0;
+    }
+
+    for (int i = 0; i < tamanhoMatriz; i++) {
+        for (int j = 0; j < tamanhoMatriz; j++) {
+            matrizResultado[i * tamanhoMatriz + j] = matriz1[i * tamanhoMatriz + j] + matriz2[i * tamanhoMatriz + j];
+        }
+    }
+
+    int sucessoEscrita = escreverMatrizNoArquivo(matrizResultado, tamanhoMatriz, nomeArquivo);
+
+    free(matrizResultado);
+
+    return sucessoEscrita;
+}
+
+int reduzindoMatriz(int *matriz, int tamanhoMatriz) {
+    int soma = 0;
+    for (int i = 0; i < tamanhoMatriz * tamanhoMatriz; i++) {
+        soma += matriz[i];
+    }
+    return soma;
+}
+
+int main() {
+    int *matrizA, *matrizB, *matrizC, *matrizD, *matrizE;
+    int tamanhoMatriz = 5;
+
+    if (!abrirArquivos("arquivos_matrizes/matrizA.dat", &matrizA, tamanhoMatriz) ||
+        !abrirArquivos("arquivos_matrizes/matrizB.dat", &matrizB, tamanhoMatriz)) {
+        printf("Erro ao abrir um ou mais arquivos de matrizA e matrizB.\n");
+    } else {
+        if (!somarMatrizes(matrizA, matrizB, tamanhoMatriz, "arquivos_matrizes/matrizD.dat")) {
+            printf("Erro ao somar as matrizes A e B e escrever no arquivo matrizD.dat.\n");
+        }
+    }
+
+    free(matrizA);
+    free(matrizB);
+
+    if (!abrirArquivos("arquivos_matrizes/matrizC.dat", &matrizC, tamanhoMatriz) ||
+        !abrirArquivos("arquivos_matrizes/matrizD.dat", &matrizD, tamanhoMatriz)) {
+        printf("Erro ao abrir um ou mais arquivos de matrizC e matrizD.\n");
+    } else {
+        if (!somarMatrizes(matrizC, matrizD, tamanhoMatriz, "arquivos_matrizes/matrizE.dat")) {
+            printf("Erro ao somar as matrizes C e D e escrever no arquivo matrizE.dat.\n");
+        }
+    }
+
+    free(matrizC);
+    free(matrizD);
+
+    if (!abrirArquivos("arquivos_matrizes/matrizE.dat", &matrizE, tamanhoMatriz)) {
+        printf("Erro ao abrir o arquivo matrizE.dat.\n");
+        return -1;
+    }
+
+    int resultado = reduzindoMatriz(matrizE, tamanhoMatriz);
+    printf("A soma de todos os elementos da matriz E é: %d\n", resultado);
+
+    free(matrizE);
 
     return 0;
 }
