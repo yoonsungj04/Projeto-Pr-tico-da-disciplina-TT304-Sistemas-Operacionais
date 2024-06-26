d77bb8c8b0d232196906eaad19172f0766ffc923
kaz-zen <lucass.hernandeez@gmail.com>
2024-06-26 09:38:01 -0300

    deleted:    lixo/manipuladorMatriz.c 	deleted:    lixo/manipuladorMatriz.h 	deleted:    lixo/operacoesMatriz.c 	deleted:    lixo/operacoesMatriz.h 	deleted:    lixo/operacoesThread.c 	deleted:    lixo/operacoesThread.h 	deleted:    threadManipularMatriz.o 	deleted:    threadOperacoesMatriz.o

--- lixo/manipuladorMatriz.h
+++
@@ -1,8 +0,0 @@
-#ifndef MANIPULADORMATRIZ_H
-#define MANIPULADORMATRIZ_H
-
-int abrirArquivos(const char* caminho, int** matriz, int tamanhoMatriz);
-
-int escreverMatrizNoArquivo(int *matriz, int tamanhoMatriz, const char *nomeArquivo);
-
-#endif 
